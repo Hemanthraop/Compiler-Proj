8373a9abb037f198fb6ff31876187b483d86c956
NikhilDevabhaktuni <61192720+NikhilDevabhaktuni@users.noreply.github.com>
2020-02-24 14:22:48 +0530

    Update time.h

--- time.h
+++ time.h
@@ -6,7 +6,29 @@
 
                 start_time = clock();
 
-                                                // invoke your lexer and parser here
+               //copy pasted from lexer main
+                intialize_symbol_table();
+
+	              struct tokenlexemepair* ex;
+	              FILE* fp=fopen("test","r");
+	              if(fp==NULL)printf("NULL file\n");
+
+	              while(1){
+		            fp=getStream(fp);
+	
+		            while(1){
+			              ex=getNexttoken(stream);
+			              if(streamover==true) break;
+			              if(iserror==true)continue;
+			              printf("%s\t%s\t%d\n",ex->token,ex->lexeme,line_no);
+		                }
+		            if(fileover==true) break;
+	              }
+                //lexer end
+
+                //parser start
+
+                //parser end
 
                 end_time = clock();
 
