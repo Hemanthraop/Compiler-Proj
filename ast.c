7958e114bd0fd9d85bcf87415f7392ca234ac616
Hemanthraop <35999039+Hemanthraop@users.noreply.github.com>
2020-04-11 19:34:58 +0530

    Update ast.c

--- ast.c
+++ ast.c
@@ -4,6 +4,7 @@ struct astnode{
 	struct astnode* Children[50];
 	int no_of_children=0;
 	char lex[21];
+	int no;
 };
 
 void reduce_mDs(struct node* root,struct astnode* ast){
@@ -34,7 +35,147 @@ void reduce_expr(struct node* root,struct astnode* ast){
 
 void reduce_arithorBool_expr(struct node* root,struct astnode* ast){
 
-	reduce_anyterm(root->Children[0],ast->Children[0]->Children[0]);
+	ast->Children[0]=malloc(sizeof(struct astnode));
+	reduce_anyterm(root->Children[0],ast->Children[0]);
+	ast->no_of_children++;
 
-	
+	if(((root->Children[1])->Children[0])->no==114){
+		//reduce_anyterm(((root->Children[1])->Children[0]),ast->Children[0]);
+	}
+	else{
+		ast->no_of_children++;
+		ast->Children[1]=malloc(sizeof(struct astnode));
+
+		//copy Lop into ast somehow
+		ast->no=(root->Children[1]->Children[0]->Children[0])->no;
+		reduce_N7(root->Children[1],ast->Children[1]);
+	}
 }
+
+void reduce_N7(struct node* root,struct astnode* ast){
+	if(((root->Children[2])->Children[0])->no==114){
+		reduce_anyterm((root->Children[1]),ast);
+	}
+	else{
+		ast->no_of_children++;
+		ast->Children[0]=malloc(sizeof(struct astnode));
+
+		reduce_anyterm((root->Children[1]),ast->Children[0]);
+		//copy Lop into ast somehow
+		ast->no=(root->Children[2]->Children[0]->Children[0])->no;
+		ast->Children[1]=malloc(sizeof(struct astnode));
+		reduce_N7(root->Children[2],ast->Children[1]);
+	}
+}
+
+void reduce_anyterm(struct node* root,struct astnode* ast){
+	if((root->Children[0])->no==56){
+		ast->no=((root->Children[0])->Children[0])->no;
+	}
+	else{
+		ast->no_of_children++;
+		ast->Children[0]=malloc(sizeof(struct astnode));
+		reduce_arith_expr(root->Children[0],ast->Children[0]);
+		if(root->Children[1]->Children[0]->no==114){
+
+		}
+		else{
+			ast->no_of_children++;
+			ast->Children[1]=malloc(sizeof(struct astnode));
+			reduce_N8(root->Children[1],ast->Children[1]);
+		}
+	}
+}
+
+void reduce_N8(struct node* root,struct astnode* ast){
+	ast->Children[0]=malloc(sizeof(struct astnode));
+	ast->no_of_children++;
+	reduce_rel_op(root->Children[0],ast->Children[0]);
+	ast->Children[1]=malloc(sizeof(struct astnode));
+	ast->no_of_children++;
+	reduce_arith_expr(root->Children[1],ast->Children[1]);
+}
+
+void reduce_rel_op(struct node* root,struct astnode* ast){
+	ast->no=(root->Children[0])->no;
+}
+
+void reduce_arith_expr(struct node* root,struct astnode* ast){
+
+	ast->Children[0]=malloc(sizeof(struct astnode));
+	reduce_term(root->Children[0],ast->Children[0]);
+	ast->no_of_children++;
+
+	if(((root->Children[1])->Children[0])->no==114){
+		//reduce_anyterm(((root->Children[1])->Children[0]),ast->Children[0]);
+	}
+	else{
+		ast->no_of_children++;
+		ast->Children[1]=malloc(sizeof(struct astnode));
+
+		//copy Lop into ast somehow
+		ast->no=(root->Children[1]->Children[0]->Children[0])->no;
+		reduce_A1(root->Children[1],ast->Children[1]);
+	}
+}
+
+void reduce_A1(struct node* root,struct astnode* ast){
+	if(((root->Children[2])->Children[0])->no==114){
+		reduce_term((root->Children[1]),ast);
+	}
+	else{
+		ast->no_of_children++;
+		ast->Children[0]=malloc(sizeof(struct astnode));
+
+		reduce_term((root->Children[1]),ast->Children[0]);
+		//copy Lop into ast somehow
+		ast->no=(root->Children[2]->Children[0]->Children[0])->no;
+		ast->Children[1]=malloc(sizeof(struct astnode));
+		reduce_A1(root->Children[2],ast->Children[1]);
+	}
+}
+
+void reduce_term(struct node* root,struct astnode* ast){
+
+	ast->Children[0]=malloc(sizeof(struct astnode));
+	reduce_factor(root->Children[0],ast->Children[0]);
+	ast->no_of_children++;
+
+	if(((root->Children[1])->Children[0])->no==114){
+		//reduce_anyterm(((root->Children[1])->Children[0]),ast->Children[0]);
+	}
+	else{
+		ast->no_of_children++;
+		ast->Children[1]=malloc(sizeof(struct astnode));
+
+		//copy op2 into ast somehow
+		ast->no=(root->Children[1]->Children[0]->Children[0])->no;
+		reduce_A2(root->Children[1],ast->Children[1]);
+	}
+}
+
+void reduce_A2(struct node* root,struct astnode* ast){
+	if(((root->Children[2])->Children[0])->no==114){
+		reduce_factor((root->Children[1]),ast);
+	}
+	else{
+		ast->no_of_children++;
+		ast->Children[0]=malloc(sizeof(struct astnode));
+
+		reduce_factor((root->Children[1]),ast->Children[0]);
+		//copy op2 into ast somehow
+		ast->no=(root->Children[2]->Children[0]->Children[0])->no;
+		ast->Children[1]=malloc(sizeof(struct astnode));
+		reduce_A2(root->Children[2],ast->Children[1]);
+	}
+}
+
+void reduce_factor(struct node* root,struct astnode* ast){
+	if(root->no_of_children==3)
+		reduce_arith_expr(root->Children[1],ast);
+	if(root->no_of_children==1){
+		reduce_var_id_num(root->Children[0],ast);
+	}
+}
+
+void reduce_
